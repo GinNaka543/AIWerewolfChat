c7c2138a12feb3820d3477bd357373a3ad3deda4
GINSEI NAKAJIMA <gn305527@falmouth.ac.uk>
2025-08-05 16:40:18 +0100

    Initial Commit

---
+++ AIWerewolfChat/AIWerewolfChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIWerewolfChatApp.swift
+//  AIWerewolfChat
+//
+//  Created by nakajimaginsei on 2025/08/05.
+//
+
+import SwiftUI
+
+@main
+struct AIWerewolfChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIWerewolfChatTests/AIWerewolfChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIWerewolfChatTests.swift
+//  AIWerewolfChatTests
+//
+//  Created by nakajimaginsei on 2025/08/05.
+//
+
+import Testing
+@testable import AIWerewolfChat
+
+struct AIWerewolfChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
